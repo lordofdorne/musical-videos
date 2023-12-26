56067ee076cb93e6d0f4bbc4de0fa990a659ca87
Asim Samuel <asim.samuel1@gmail.com>
2023-12-26 16:23:30 -0500

    scaffold app, setup docker

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+const app = express();
+app.use(express.json())
+
+app.post("/process-video", (req, res) => {
+  // Get path of input video file from the body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+        res.status(200).send("Video processing complete.")
+    })
+    .on("error", (err) => {
+      console.log(`An error occured: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
+    
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`video processing service listening at http://localhost:${port}`);
+});
